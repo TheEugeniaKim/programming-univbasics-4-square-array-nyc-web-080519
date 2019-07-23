ef7dd72e65b8c5891cf37b108efbae26377d4918
Eugenia Kim <elk303@nyu.edu>
2019-07-23 23:51:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@
 def square_array(array)
   counter = 0
   newarray = []
-  while counter < array.length do
+  while counter < array.length-1 do
     array.push(array[counter]**2)
     counter += 1
   end
